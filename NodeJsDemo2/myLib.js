18afb4e2f139ac5648fe77028f1c43f49ebb1642
vtienhieu <tienhieucn@gmail.com>
2020-07-14 11:59:54 +0700

    ex

---
+++ NodeJsDemo2/myLib.js
@@ -0,0 +1,18 @@
+var fun = {
+  sumN: function sumN(n) {
+    let s = 0;
+    for (i = 1; i <= n; i++) {
+      s = s + i;
+    }
+    return s;
+  },
+
+  gtN: function gtN(n) {
+    let s = 1;
+    for (i = 1; i <= n; i++) {
+      s = s * i;
+    }
+    return s;
+  },
+};
+module.exports = fun;
